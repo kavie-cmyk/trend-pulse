c6b4ba9023d30594e6b8ba72491ac93fd40af54d
kavie-cmyk <anhthu.tang2004@gmail.com>
2026-08-29 15:55:54 +0700

    feat: render social signal backbone

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import BackboneSignals from "./backbone-signals";
 import IntelligenceOutput from "./intelligence-output";
 import LiveSignals from "./live-signals";
+import SocialSignals from "./social-signals";
 import SourceIntelligencePanel from "./source-intelligence-panel";
 import WorkspaceConsole from "./workspace-console";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <main className="shell">
       <WorkspaceConsole />
       <SourceIntelligencePanel />
+      <SocialSignals />
       <BackboneSignals />
       <LiveSignals />
       <IntelligenceOutput />
