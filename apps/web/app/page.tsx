3d4736956d7a25a1bbd38c16621d400e790a0ebb
kavie-cmyk <anhthu.tang2004@gmail.com>
2026-08-30 22:29:16 +0700

    feat: render Stage 04G paid creative bridge

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import WorkspaceConsole from "./workspace-console";
 import WorkspaceIntelligence04F from "./workspace-intelligence-04f";
+import PaidCreativeStage04G from "./paid-creative-stage-04g";
 import BrandFitStage05B from "./brand-fit-stage-05b";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="shell">
       <WorkspaceConsole />
       <WorkspaceIntelligence04F />
+      <PaidCreativeStage04G />
       <BrandFitStage05B />
     </main>
   );
